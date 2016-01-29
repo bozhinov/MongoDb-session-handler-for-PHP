62456522f89f66b5805a34ab1b1673bd4b245299
Momchil Bozhinov <momchil@bojinov.info>
2016-01-29 22:59:06 +0200

    * fixed tabulation

--- MongoSession.php
+++ MongoSession.php
@@ -9,43 +9,43 @@ class MongoSession implements SessionHandlerInterface {
 
     public function __construct(){
 
-        session_set_save_handler(
-            array($this, 'open'),
-            array($this, 'close'),
-            array($this, 'read'),
-            array($this, 'write'),
-            array($this, 'destroy'),
-            array($this, 'gc')
-        );
+		session_set_save_handler(
+			array($this, 'open'),
+			array($this, 'close'),
+			array($this, 'read'),
+			array($this, 'write'),
+			array($this, 'destroy'),
+			array($this, 'gc')
+		);
 
 		// Uncomment all needed that do not match defaults in php.ini
-        #ini_set('session.auto_start',               0);
-        #ini_set('session.gc_probability',           1);
-        #ini_set('session.gc_divisor',               100);
-        ini_set('session.gc_maxlifetime',           14400); # session lifetime in seconds
-        #ini_set('session.referer_check',            '');
-        #ini_set('session.entropy_file',             '/dev/urandom');   
-        #ini_set('session.entropy_length',           16);
-        #ini_set('session.use_cookies',              1);
-        #ini_set('session.use_only_cookies',         1);
-        #ini_set('session.use_trans_sid',            0);
-        #ini_set('session.hash_function',            1);
-        #ini_set('session.hash_bits_per_character',  5);
+		#ini_set('session.auto_start',               0);
+		#ini_set('session.gc_probability',           1);
+		#ini_set('session.gc_divisor',               100);
+		ini_set('session.gc_maxlifetime',           14400); # session lifetime in seconds
+		#ini_set('session.referer_check',            '');
+		#ini_set('session.entropy_file',             '/dev/urandom');   
+		#ini_set('session.entropy_length',           16);
+		#ini_set('session.use_cookies',              1);
+		#ini_set('session.use_only_cookies',         1);
+		#ini_set('session.use_trans_sid',            0);
+		#ini_set('session.hash_function',            1);
+		#ini_set('session.hash_bits_per_character',  5);
 		#ini_set('session.cookie_domain',  $_SERVER['SERVER_NAME']);
 
-        session_cache_limiter('nocache');
-        session_set_cookie_params(
+		session_cache_limiter('nocache');
+		session_set_cookie_params(
 			14400, #  session lifetime in seconds
 			'/', # 'cookie_path'
 			'' #$_SERVER['SERVER_NAME']
 		);
-        session_name('mongo_sess');
-    
-        register_shutdown_function('session_write_close');
-		
+		session_name('mongo_sess');
+
+		register_shutdown_function('session_write_close');
+
 		$this->_init();
 
-        session_start();
+		session_start();
 	}
 
     /**
@@ -70,7 +70,7 @@ class MongoSession implements SessionHandlerInterface {
 
     public function close(){
 		unset($this->_mongo);
-        return true;
+		return true;
     }
 
     /**
@@ -99,40 +99,41 @@ class MongoSession implements SessionHandlerInterface {
      */
     public function write($id, $data){
 		
-        // create new session data
-        $new_obj = array(
-            'data' => $data,
-            'active' => 1,
-            'expiry' => time() + 14400 # session lifetime in seconds
-        );
-        
-        // check for existing session for merge
-        if (!empty($this->_session)) {
-            $new_obj = array_merge((array)$this->_session, $new_obj);
-        }
-        unset($new_obj['_id']);
-        		  
+		// create new session data
+		$new_obj = array(
+			'data' => $data,
+			'active' => 1,
+			'expiry' => time() + 14400 # session lifetime in seconds
+		);
+
+		// check for existing session for merge
+		if (!empty($this->_session)) {
+			$new_obj = array_merge((array)$this->_session, $new_obj);
+		}
+		unset($new_obj['_id']);
+				  
 		// perform the update or insert
 		try {
 			$result = $this->_mongo->update(array('session_id' => $id), array('$set' => $new_obj), array('upsert'=> TRUE)); 
 			return $result['ok'] == 1;
 		} catch (Exception $e) {
-            die($e);
+			die($e);
 		}
-		
-        return true;
-    }
-    /**
-     * Destroys the session by removing the document with matching session_id.
-     */
-    public function destroy($id){
-        $this->_mongo->remove(array('session_id' => $id));
-        return true;
-    }
 
-    /**
-     * Garbage collection. Remove all expired entries atomically.
-     */
+		return true;
+    }
+	
+	/**
+	 * Destroys the session by removing the document with matching session_id.
+	 */
+	public function destroy($id){
+		$this->_mongo->remove(array('session_id' => $id));
+		return true;
+	}
+
+	/**
+	 * Garbage collection. Remove all expired entries atomically.
+	 */
 	public function gc($maxLifeTime = 3600){
 		// update expired elements and set to inactive
 		$this->_mongo->update(
@@ -142,7 +143,7 @@ class MongoSession implements SessionHandlerInterface {
 		);
 
 		return true;
-   	}
+	}
 	
 	/**
 	 * Solves issues with write() and close() throwing exceptions.
